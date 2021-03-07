bf14f1ed2be9f222437aba1b1fe984541cf8179c
Bruno Cochofel <bruno.cochofel@gmail.com>
2021-03-07 17:04:36 +0000

    fix: missing vars for vnet module

--- main.tf
+++ main.tf
@@ -13,9 +13,12 @@ module "vnet" {
   source  = "bcochofel/virtual-network/azurerm"
   version = "~> 1.2.0"
 
-  resource_group_name         = module.rg.name
-  name                        = var.virtual_network_name
-  address_space               = var.address_space
+  resource_group_name   = module.rg.name
+  name                  = var.virtual_network_name
+  address_space         = var.address_space
+  dns_servers           = var.dns_servers
+  vm_protection_enabled = var.vm_protection_enabled
+
   enable_ddos_pp              = var.enable_ddos_pp
   create_ddos_pp              = var.create_ddos_pp
   ddos_pp_name                = var.ddos_pp_name
